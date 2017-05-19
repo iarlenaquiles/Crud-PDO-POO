15aed4f25897224cae3435ebc3d2a27b62cffa09
junior <aefs12junior@gmail.com>
2017-05-19 18:56:47 -0300

    Atualização do README

--- classes/DB.php
+++ classes/DB.php
@@ -11,8 +11,8 @@ class DB
 		if(!isset(self::$instance)){
 
 			try {
-				
-				self::$instance = new PDO('mysql:host=localhost;dbname=bancodedados', "root", "root");
+
+				self::$instance = new PDO('mysql:host=localhost;dbname=bancodedados', "", "");
 				self::$instance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
