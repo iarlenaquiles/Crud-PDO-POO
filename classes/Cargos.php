bfc4bfa20a349f98fe69ae6a08dad24047abf9ed
junior <aefs12junior@gmail.com>
2017-05-19 18:53:30 -0300

    Refatoração básica dos códigos

--- classes/Cargos.php
+++ classes/Cargos.php
@@ -1,31 +1,37 @@
 <?php
 
-require_once 'Crud.class.php';
+require_once 'Crud.php';
 
-class Cargos extends Crud{
+class Cargos extends Crud
+{
 
 	protected $table = 'cargos';
 	private $nome;
 
-	public function setNome($nome){
+	public function setNome($nome)
+	{
 		$this->nome = $nome;
 	}
 
-	public function insert(){
-
+	public function insert()
+	{
 		$sql  = "INSERT INTO $this->table (nome) VALUES (:nome)";
 		$stmt = DB::prepare($sql);
+
 		$stmt->bindValue(':nome', $this->nome);
+
 		return $stmt->execute();
 
 	}
 
-	public function update($id){
-
+	public function update($id)
+	{
 		$sql  = "UPDATE $this->table SET nome = :nome WHERE id = :id";
 		$stmt = DB::prepare($sql);
+
 		$stmt->bindValue(':nome', $this->nome);
 		$stmt->bindValue(':id', $id);
+
 		return $stmt->execute();
 
 	}
