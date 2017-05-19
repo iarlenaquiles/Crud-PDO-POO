fb3eafdddb3ed9ccb26951af5e6fe8ad5fd1a685
junior <aefs12junior@gmail.com>
2017-05-19 19:12:30 -0300

    Refatoração nos arquivos de view

--- view/listar_Cargos.php
+++ view/listar_Cargos.php
@@ -7,15 +7,15 @@
 		</tr>
 
 <?php
-	 foreach($cargo->findAll() as $key => $value): ?>
+	foreach($cargo->findAll() as $key => $value): ?>
 		<tr>
 			<td><?= $value->id; ?></td>
 			<td><?= $value->nome; ?></td>
 			<td>
-				<?= "<a href='../index.php?acao=editar_cargo&id=" . $value->id . "'>Editar</a>"; ?>
-				<?= "<a href='../index.php?acao=deletar_cargo&id=" . $value->id . "' onclick='return confirm(\"Deseja realmente deletar?\")'>Deletar</a>"; ?>
+				<a href='?acao=editar_cargo&id=<?= $value->id; ?>'>Editar</a>
+				<a href='?acao=deletar_cargo&id=<?= $value->id; ?>' onclick='return confirm(\"Deseja realmente deletar?\")'>Deletar</a>
 			</td>
-<?php endforeach; ?>
+	<?php endforeach; ?>
 		</tr>
 	</table>
 </div>
