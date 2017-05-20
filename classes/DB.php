6d3021c87439edb6aa8204dacc20dc4ffe054c8c
Jholdy Damasceno <jholdydamasceno@gmail.com>
2017-05-20 06:24:05 -0300

    Correção classe DB, deixando default.

--- classes/DB.php
+++ classes/DB.php
@@ -8,7 +8,7 @@ class DB
     {
         if (!isset(self::$instance)) {
             try {
-                self::$instance = new PDO('mysql:host=localhost;dbname=bancodedados', "root", "123123");
+                self::$instance = new PDO('mysql:host=localhost;dbname=bancodedados', "", "");
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             } catch (PDOException $e) {
